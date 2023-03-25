56b88bd27f607864f13d57e23e7915af58a150f2
mwezn <m.w.g.nelson@gmail.com>
2023-03-25 18:56:52 +0000

    Added usestate to keep track of mental arithmetic questions

--- src/components/question.js
+++ src/components/question.js
@@ -2,7 +2,10 @@
 Writing my own code from scratch for a mental arithmetic app
 */ 
 
+import React from 'react'
+
 function Question(){
+    const [data, setData] = React.useState({question_no:1, num_of_questions:10,question:q2()})
 
     function random(){
         return Math.floor(10+Math.random()*100)
@@ -16,15 +19,20 @@ function Question(){
     let question= q2();
     const handleInput=(e)=>{
         console.log(e.target.value)
-        if(e.target.value==question.answer){
+        if(e.target.value==data.question.answer){
             console.log('CORRECT!')
-            window.location.href='/'
+            e.target.value=''
+            const newObj = { question_no: data.question_no+=1,question:q2()}
+            setData(old=>{
+                return {...old, ...newObj}
+            })
         }
     }
 
     return (
     <div>
-        <h1>{question.question}</h1>
+        <h1>{data.question_no}</h1>
+        <h3>{data.question.question}</h3>
         <input onInput={handleInput}></input>
     </div>)
 }
